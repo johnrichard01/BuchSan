bffc49af08be04e07acc26206a772146de53b768
johnrichard01 <pro.richardsumagui001@gmail.com>
2023-11-20 19:06:16 +0800

    added signup css and js with validation

---
+++ Home/signup.js
@@ -0,0 +1,114 @@
+
+function isValidEmail(email) {
+    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  }
+function isValidPassword(password) {
+    var passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
+    return passwordRegex.test(password);
+}
+function passwordsMatch(password, confirmPassword) {
+    return password === confirmPassword;
+  }
+
+function validate(event){
+    let fullnameVal = document.getElementById("fullname").value;
+    let usernameVal = document.getElementById("username").value;
+    let emailVal = document.getElementById("email").value;
+    let passwordVal = document.getElementById("password").value;
+    let confirmpasswordVal = document.getElementById("confirmpassword").value;
+    let empty="";
+    let isValid=true;
+
+    if (fullnameVal==empty){
+        event.preventDefault();
+        document.getElementById("error-message1").classList.add("error-print");
+        document.getElementById("infieldError1").classList.add("in-field-invalid");
+        isValid=false;
+    } if (usernameVal==empty){
+        event.preventDefault();
+        document.getElementById("error-message2").classList.add("error-print");
+        document.getElementById("infieldError2").classList.add("in-field-invalid");
+    } if (emailVal==empty){
+        event.preventDefault();
+        document.getElementById("error-message3").classList.add("error-print");
+        document.getElementById("infieldError3").classList.add("in-field-invalid");
+        isValid=false;
+    } if (!isValidEmail(emailVal)){
+        event.preventDefault();
+        document.getElementById("error-message3").classList.add("error-print");
+        document.getElementById("infieldError3").classList.add("in-field-invalid");
+        isValid=false;
+    } if(passwordVal==empty){
+        event.preventDefault();
+        document.getElementById("error-message4").classList.add("error-print");
+        document.getElementById("infieldError4").classList.add("in-field-invalid");
+        isValid=false;
+    } if(!isValidPassword(passwordVal)){
+        event.preventDefault();
+        document.getElementById("error-message4").classList.add("error-print");
+        document.getElementById("infieldError4").classList.add("in-field-invalid");
+        isValid=false;
+    } if(confirmpasswordVal==empty){
+        event.preventDefault();
+        document.getElementById("error-message5").classList.add("error-print");
+        document.getElementById("infieldError5").classList.add("in-field-invalid");
+        isValid=false;
+    } if(!passwordsMatch(passwordVal, confirmpasswordVal)){
+        event.preventDefault();
+        document.getElementById("error-message5").classList.add("error-print");
+        document.getElementById("infieldError5").classList.add("in-field-invalid");
+        isValid=false;
+    } if(isValid){
+        saveData();
+        document.getElementById("fullname").value="";
+        document.getElementById("username").value="";
+        document.getElementById("email").value="";
+        document.getElementById("password").value="";
+        document.getElementById("confirmpassword").value="";
+        message.innerHTML="";
+    }
+}
+
+document.getElementById("fullname").addEventListener("input",function(){
+    document.getElementById("error-message1").classList.remove("error-print");
+    document.getElementById("infieldError1").classList.remove("in-field-invalid");
+});
+document.getElementById("username").addEventListener("input",function(){
+    document.getElementById("error-message2").classList.remove("error-print");
+    document.getElementById("infieldError2").classList.remove("in-field-invalid");
+});
+document.getElementById("email").addEventListener("input",function(){
+    document.getElementById("error-message3").classList.remove("error-print");
+    document.getElementById("infieldError3").classList.remove("in-field-invalid");
+});
+document.getElementById("password").addEventListener("input",function(){
+    document.getElementById("error-message4").classList.remove("error-print");
+    document.getElementById("infieldError4").classList.remove("in-field-invalid");
+});
+document.getElementById("confirmpassword").addEventListener("input",function(){
+    document.getElementById("error-message5").classList.remove("error-print");
+    document.getElementById("infieldError5").classList.remove("in-field-invalid");
+    let passwordVal=document.getElementById("password").value;
+    let confirmpasswordVal=document.getElementById("confirmpassword").value;
+    let message=document.getElementById("message")
+
+    if(passwordVal===confirmpasswordVal){
+        message.innerHTML="Password match.";
+        message.style.color="green";
+    } else {
+        message.innerHTML="Password does not match.";
+        message.style.color="red";
+    }
+});
+
+function saveData(){
+    let name, username, email, password;
+    name=document.getElementById("fullname").value;
+    username=document.getElementById("username").value;
+    email=document.getElementById("email").value;
+    password=document.getElementById("password").value;
+    console.log(name+username+email+password);
+}
+
+document.getElementById("submit").addEventListener("click",validate);
