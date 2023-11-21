e08525da6b40765abea8ad848974567150481940
johnrichard01 <pro.richardsumagui001@gmail.com>
2023-11-21 21:33:11 +0800

    added login and admin login

--- Home/login.js
+++ Home/login.js
@@ -1,16 +1,40 @@
+
+
+
 function saveData(){
     let email=document.getElementById("emailLogin").value;
     let password=document.getElementById("passwordLogin").value;
 
     let user_records=new Array();
     user_records=JSON.parse(localStorage.getItem("users"))?JSON.parse(localStorage.getItem("users")):[];
+    let admin_records=new Array();
+    admin_records = JSON.parse(localStorage.getItem("admins"))?JSON.parse(localStorage.getItem("admins")):[];
 
-    if(user_records.some(function(v){
-        return v.email==email && v.password==password
-    })){
-        alert("login successful")
+
+    if (!email || !password) {
+        document.getElementById("error-enter").innerHTML="Please enter both email and password";
+        return;
+    }
+
+    let matchedUser = user_records.some(function (v) {
+        return v.email === email && v.password === password;
+    });
+
+    let matchedAdmin = admin_records.some(function (v) {
+        return v.email === email && v.password === password;
+    });
+
+    if(matchedAdmin){
+        let current_admin=admin_records.filter(function(v){
+            return v.email=== email && v.password === password;
+        })[0]
+        localStorage.setItem("name", current_admin.name);
+        localStorage.setItem("username", current_admin.username);
+        localStorage.setItem("email", current_admin.email);
+        window.location.href="../admin.html";
+    }else if(matchedUser){
         let current_user=user_records.filter(function(v){
-            return v.email==email && v.password==password;
+            return v.email===email && v.password===password;
         })[0]
         localStorage.setItem("name", current_user.name);
         localStorage.setItem("username", current_user.username);
@@ -26,10 +50,12 @@ document.getElementById("emailLogin").addEventListener("input",function(){
     document.getElementById("error-message").classList.remove("error-print");
     document.getElementById("in-group1").classList.remove("in-field-invalid");
     document.getElementById("in-group2").classList.remove("in-field-invalid");
+    document.getElementById("error-enter").innerHTML="";
 })
 document.getElementById("passwordLogin").addEventListener("input",function(){
     document.getElementById("error-message").classList.remove("error-print");
     document.getElementById("in-group1").classList.remove("in-field-invalid");
     document.getElementById("in-group2").classList.remove("in-field-invalid");
+    document.getElementById("error-enter").innerHTML="";
 })
 document.getElementById("loginButton").addEventListener("click", saveData);
--- Home/signup.js
+++ Home/signup.js
@@ -1,3 +1,22 @@
+//set admin accounts
+let admin_records=new Array();
+admin_records = JSON.parse(localStorage.getItem("admins"))?JSON.parse(localStorage.getItem("admins")):[];
+admin_records.push({
+    "name": "jessica Pam Velasquez",
+    "username": "pam",
+    "email":"pam@buchsan.com" ,
+    "password":"12345678"
+})
+admin_records.push({
+    "name": "John Richard Vidal Sumagui",
+    "username": "Richard",
+    "email":"richard@buchsan.com" ,
+    "password":"12345678"
+})
+localStorage.setItem("admins",JSON.stringify(admin_records));
+
+
+
 //email validation
 function isValidEmail(email) {
     var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
