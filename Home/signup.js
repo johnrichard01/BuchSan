b5f99634712fd2148abaac04ca407dbae5dc03ce
johnrichard01 <pro.richardsumagui001@gmail.com>
2023-11-21 21:46:59 +0800

    added admin data

--- Home/signup.js
+++ Home/signup.js
@@ -1,19 +1,4 @@
-//set admin accounts
-let admin_records=new Array();
-admin_records = JSON.parse(localStorage.getItem("admins"))?JSON.parse(localStorage.getItem("admins")):[];
-admin_records.push({
-    "name": "jessica Pam Velasquez",
-    "username": "pam",
-    "email":"pam@buchsan.com" ,
-    "password":"12345678"
-})
-admin_records.push({
-    "name": "John Richard Vidal Sumagui",
-    "username": "Richard",
-    "email":"richard@buchsan.com" ,
-    "password":"12345678"
-})
-localStorage.setItem("admins",JSON.stringify(admin_records));
+
 
 
 
--- index.js
+++ index.js
@@ -1,16 +1,37 @@
 //set admin accounts
 let admin_records=new Array();
 admin_records = JSON.parse(localStorage.getItem("admins"))?JSON.parse(localStorage.getItem("admins")):[];
-admin_records.push({
+// Check if the data already exists in the array
+var newData = {
     "name": "jessica Pam Velasquez",
     "username": "pam",
-    "email":"pam@buchsan.com" ,
-    "password":"12345678"
-})
-admin_records.push({
+    "email": "pam@buchsan.com",
+    "password": "12345678"
+};
+
+var newDataExists = admin_records.some(function (admin) {
+    return admin.username === newData.username && admin.email === newData.email;
+});
+
+if (!newDataExists) {
+    // Data doesn't exist, so push it into the array
+    admin_records.push(newData);
+}
+
+// Repeat the process for the second set of data
+newData = {
     "name": "John Richard Vidal Sumagui",
     "username": "Richard",
-    "email":"richard@buchsan.com" ,
-    "password":"12345678"
-})
+    "email": "richard@buchsan.com",
+    "password": "12345678"
+};
+
+newDataExists = admin_records.some(function (admin) {
+    return admin.username === newData.username && admin.email === newData.email;
+});
+
+if (!newDataExists) {
+    // Data doesn't exist, so push it into the array
+    admin_records.push(newData);
+}
 localStorage.setItem("admins",JSON.stringify(admin_records));
